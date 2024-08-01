a20447e00e5b45de857d6eb8407f4ce667d96a13
Vala <peshkova-vap@yansex.ru>
2024-08-01 09:33:13 +0300

    Add changelogs

--- bild-and-run -liquibase.sh
+++ bild-and-run -liquibase.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#Создание директории
-mkdir -p changelogs
-
 #Создание образа liquibase
 docker build -t my-liquibase .
 
-# Генерация changelog
-docker run --rm -v $(pwd):/liquibase -w /liquibase my-liquibase-image \
-    generateChangeLog --changeLogFile=changelogs/dbchangelog.xml
